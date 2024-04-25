6ebc9c0fd63ac7297c137e2ea753be00b8dea57c
JhojanBinary <102551448+JhojanBinary@users.noreply.github.com>
2024-04-25 22:09:32 +0000

    landing page react

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -9,7 +9,7 @@ export function Card() {
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text"> thSome quick example text to build on the card title and make up th Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn btn-primary">Go somhere</a>
                     </div>
             </div>
 
